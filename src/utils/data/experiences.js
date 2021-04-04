57f3425bb10b611c36c6d8e0d221a2b9155b22b1
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2021-04-04 22:45:58 +0300

    experiences sayfasına içerik eklendi

---
+++ src/utils/data/experiences.js
@@ -0,0 +1,12 @@
+export const experiences =
+    [{
+        "companyName": "Phexum Yazılım Ltd. Şti",
+        "date": "Feb 2021 - May 2021 ",
+        "description": "This long-term internship takes place between the dates of 15.02.2021-28.05.2021.Using Spring Boot and Vue technologies, awe developed a web application.",
+        "position": "Intern"
+    }, {
+        "companyName": "Phexum Yazılım Ltd. Şti",
+        "date": "Jun 2019 - July 2019 ",
+        "description": "We developed an android application in Java language, I developed a spam mail management script in the JavaScript language and I learned to use Docker.",
+        "position": "Intern"
+    }]
--- src/views/Experiences.vue
+++ src/views/Experiences.vue
@@ -3,44 +3,19 @@
     <div class="fh5co-content-inner">
       <div class="row">
         <div class="col-md-12">
-          <p>I have the privilege to work with these cool companies.</p>
-
-          <div class="fh5co-feature">
+          <div v-for="ex in experiences" :key="ex" class="fh5co-feature">
             <div class="fh5co-icon">
               <i class="icon-black-tie"></i>
             </div>
             <div class="fh5co-text">
-              <h2>2008 - Present</h2>
-              <h3>XYZ Inc.</h3>
+              <h2>{{ ex.date }}</h2>
+              <hr />
+              <h3>{{ ex.companyName }}</h3>
+              <h4 class="gold">{{ ex.position }}</h4>
               <p>
-                Far far away, behind the word mountains, far from the countries.
+                {{ ex.description }}
               </p>
-            </div>
-          </div>
-
-          <div class="fh5co-feature">
-            <div class="fh5co-icon">
-              <i class="icon-black-tie"></i>
-            </div>
-            <div class="fh5co-text">
-              <h2>2004-2008</h2>
-              <h3>Previous Ltd Co.</h3>
-              <p>
-                Far far away, behind the word mountains, far from the countries.
-              </p>
-            </div>
-          </div>
-
-          <div class="fh5co-feature">
-            <div class="fh5co-icon">
-              <i class="icon-black-tie"></i>
-            </div>
-            <div class="fh5co-text">
-              <h2>1999-2004</h2>
-              <h3>Previous Ltd Co.</h3>
-              <p>
-                Far far away, behind the word mountains, far from the countries.
-              </p>
+              <br />
             </div>
           </div>
         </div>
@@ -49,7 +24,18 @@
   </div>
 </template>
 <script>
+import { experiences } from "../utils/data/experiences";
 export default {
   name: "experiences",
+  data() {
+    return {
+      experiences: experiences,
+    };
+  },
 };
 </script>
+<style scoped>
+.gold {
+  color: gold;
+}
+</style>
